709e496a9067b1dbd5273907b45b9cc713f3cbf3
Patrick <shantry@hotmail.fr>
2021-05-30 15:04:48 +0200

    Add top messages icon

--- webapp/src/containers/topMessages/index.js
+++ webapp/src/containers/topMessages/index.js
@@ -1,3 +1,8 @@
+import {
+  faBan,
+  faExclamationTriangle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSocketIsConnected } from 'containers/socket';
 import { useRunnerAvailable } from 'hooks';
 
@@ -9,11 +14,13 @@ export const TopMessages = () => {
     <div>
       {isSocketConnected === false && (
         <div className="bg-red-500 text-white p-2 text-sm">
+          <FontAwesomeIcon className="mr-1" icon={faBan} />
           Not connected to server
         </div>
       )}
       {runnerAvailable === false && (
         <div className="bg-red-500 text-white p-2 text-sm">
+          <FontAwesomeIcon className="mr-1" icon={faExclamationTriangle} />
           No runner available. Start runner with{' '}
           <code className="bg-gray-600 rounded p-1">npx metro-runner</code>
         </div>
