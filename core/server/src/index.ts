741a8737ae46792abe9038695c775fde14279b78
shantlr <20406266+shantlr@users.noreply.github.com>
2021-12-09 22:51:44 +0100

    Add watch config files

--- core/server/src/index.ts
+++ core/server/src/index.ts
@@ -1,14 +1,49 @@
+import { toLower } from 'lodash';
+import gaze from 'gaze';
+import path from 'path';
+
 import { startApi } from './api';
 import { config } from './config';
 import { doMigrations } from './data';
 import { EVENTS, EventManager } from './events';
 import { startRunnerWs } from './runnerWs';
+import { createLogger } from '@shantr/metro-logger';
 
 const main = async (): Promise<void> => {
   await doMigrations();
 
-  EventManager.startConsumeEvent();
   EventManager.push(EVENTS.load.dir(config.get('configs.path')));
+
+  if (toLower(config.get('configs.watch')) === 'true') {
+    const watchLogger = createLogger('file-watcher');
+
+    gaze(
+      [
+        `${config.get('configs.path')}/**/*.yaml`,
+        `${config.get('configs.path')}/**/*.yml`,
+      ],
+      {},
+      function (err, watcher) {
+        if (err) {
+          watchLogger.error(err.message);
+        } else {
+          this.on('added', (filepath: string) => {
+            EventManager.push(EVENTS.load.file(filepath));
+          });
+          this.on('deleted', (filepath: string) => {
+            console.log('FILE REMOVED', filepath);
+          });
+          this.on('changed', (filepath: string) => {
+            EventManager.push(EVENTS.load.file(filepath));
+          });
+        }
+      }
+    );
+    watchLogger.info(
+      `watching config files in '${path.resolve(config.get('configs.path'))}'`
+    );
+  }
+  EventManager.startConsumeEvent();
 
   await startRunnerWs();
   await startApi(config.get('api.port'));
